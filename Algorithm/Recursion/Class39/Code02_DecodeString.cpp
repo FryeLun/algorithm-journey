67c1959644575ef8938c7c5cd22ac223402a2b8f
FryeLun <894750370@qq.com>
2024-07-21 22:11:16 +0800

    嵌套递归

--- Algorithm/Recursion/Class39/Code02_DecodeString.cpp
+++ Algorithm/Recursion/Class39/Code02_DecodeString.cpp
@@ -2,3 +2,39 @@
 // 测试链接 : https://leetcode.cn/problems/decode-string/
 #include <bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+    int where = 0;
+
+    string decodeString(string s) {
+        where  = 0;
+        return f(s, 0);
+    }
+
+    string f(string &s, int i){
+        int num = 0;
+        string ans;
+        while(i < s.size() && s[i] != ']'){
+            if(s[i] >= '0' && s[i] <= '9'){
+                num = num * 10 + s[i++] - '0';
+            }else if(s[i] >= 'a' && s[i] <= 'z'){
+                ans += s[i++];
+            }else{
+                ans += get(f(s, i + 1), num);
+                i = where + 1;
+                num = 0;
+            }
+        }
+        where = i;
+        return ans;
+    }
+
+    string get(string str, int n){
+        string ans;
+        for(int i = 0;i < n;i++){
+            ans += str;
+        }
+        return ans;
+    }
+};
--- Algorithm/Recursion/Class39/Code03_NumberOfAtoms.cpp
+++ Algorithm/Recursion/Class39/Code03_NumberOfAtoms.cpp
@@ -2,3 +2,62 @@
 // 测试链接 : https://leetcode.cn/problems/number-of-atoms/
 #include <bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+    int where;
+
+    string countOfAtoms(string formula) {
+        where = 0;
+        map<string, int> m = f(formula, 0);
+        string ans;
+        for(auto& entry : m){
+            ans += entry.first;
+            if(entry.second > 1){
+                ans += to_string(entry.second);
+            }
+        }
+        return ans;
+    }
+
+    map<string, int> f(string& s, int i){
+        map<string, int> ans;
+        map<string, int> pre;
+        string name;
+        int cnt = 0;
+        while(i < s.size() && s[i] != ')'){
+            if(s[i] >= '0' && s[i] <= '9'){
+                cnt = cnt * 10 + s[i++] -'0';
+            }else if(s[i] >= 'A' && s[i] <= 'Z' || s[i] == '('){
+                fillFormula(ans, name, pre, cnt);
+                name.clear();
+                pre.clear();
+                cnt = 0;
+                if(s[i] >= 'A' && s[i] <= 'Z'){
+					name += s[i++];
+				}else{
+					pre = f(s, i + 1);
+					i = where + 1;
+				}
+            }else{
+                name += s[i++];
+            }
+        }
+        fillFormula(ans, name, pre, cnt);
+        where = i;
+        return ans;
+    }
+
+    void fillFormula(map<string, int>& ans,string& name, map<string, int>& pre,int cnt){
+        if(!name.empty() || !pre.empty()){
+            cnt = cnt == 0 ? 1 : cnt;
+            if (!name.empty()) {
+                ans[name] += cnt;
+            } else {
+                for (auto& key : pre) {
+                    ans[key.first] += key.second * cnt;
+                }
+            }
+        }
+    }
+};
